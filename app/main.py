7add7ccda3b6bd68849316683e885322d873b102
rxavier1904 <rmbx@cin.ufpe.br>
2025-11-19 16:29:17 -0300

    updated the routes and renamed intens_pedidos_services.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
-from app.routers import example_router, pedidos_router
+from app.routers import example_router, pedidos_router, itens_pedidos, vendedores
 from app.routers.produtos_router import router as produtos_router
 
 app = FastAPI(
@@ -17,6 +17,7 @@ async def read_root():
 async def health_check():
     return {"status": "ok"}
 
-app.include_router(example_router, prefix="/example", tags=["Example"])
 app.include_router(produtos_router)
 app.include_router(pedidos_router, prefix="/pedidos", tags=["Pedidos"])
+app.include_router(itens_pedidos.router)
+app.include_router(vendedores.router)
