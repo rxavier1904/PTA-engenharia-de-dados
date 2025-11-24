9ea627d171672728047b557841961283f16afbb8
Davi Peixoto <davipffalcao@gmail.com>
2025-11-23 22:39:36 -0300

    fix/atualizacao do schemas/produtos

--- app/schemas/produtos.py
+++ app/schemas/produtos.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
-
+from typing import Optional
 
 class ProdutoRaw(BaseModel):
     product_id: str
     product_category_name: str
-    product_name_lenght: float
-    product_description_lenght: float
-    product_photos_qty: float
-    product_weight_g: float
-    product_length_cm: float
-    product_height_cm: float
-    product_width_cm: float
+    product_name_lenght: Optional[float] = None
+    product_description_lenght: Optional[float] = None
+    product_photos_qty: Optional[float] = None
+    product_weight_g: Optional[float] = None
+    product_length_cm: Optional[float] = None
+    product_height_cm: Optional[float] = None
+    product_width_cm: Optional[float] = None
